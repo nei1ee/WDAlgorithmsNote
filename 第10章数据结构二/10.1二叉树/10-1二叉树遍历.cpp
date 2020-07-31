d055908c84cac2f5eb008c746c8e9bed0d3de262
lazylzg <lizhiguo@cumt.edu.cn>
2020-07-31 09:33:43 +0800

    Update 10-1二叉树遍历.cpp

--- 第10章数据结构二/10.1二叉树/10-1二叉树遍历.cpp
+++ 第10章数据结构二/10.1二叉树/10-1二叉树遍历.cpp
@@ -1,45 +1,42 @@
 #include<iostream>
-#include<algorithm>
-#include<string.h>
 using namespace std;
-/**二叉树遍历 
-*思路：重点在于根据题目要求建立一棵树 
-*此外便是对应的进行访问，这点不难 
-*/
 
-struct Node{
-	char data;
-	Node* left;
-	Node* right;
-	Node(char c):data(c),left(NULL),right(NULL){};
+struct TreeNode {
+    char data;
+    TreeNode *leftChild;
+    TreeNode *rightChild;
+    TreeNode(char c):data(c),leftChild(NULL),rightChild(NULL) {};
 };
-//从给定输入字符串开始构建树，返回根节点 
-Node* BuildTree(int &position,const string &str){
-	char c=str[position++];
-	if(c=='#')return NULL;
-	Node *node = new Node(c);
-	node->left = BuildTree(position,str);
-	node->right = BuildTree(position,str);
-	return node;
+
+TreeNode* Build(int &position,string str) {
+    char c=str[position++];
+    if(c=='#') {
+        return NULL;
+    }
+    TreeNode *root=new TreeNode(c);
+    root->leftChild=Build(position,str);
+    root->rightChild=Build(position,str);
+    return root;
 }
 
-void InOrder(Node* root){
-	if(root == NULL)return ;
-	InOrder(root->left);
-	cout<<root->data<<" ";
-	InOrder(root->right);
-	return ;
-} 
+void InOrder(TreeNode *root) {
+    if(root==NULL) {
+        return;
+    }
+    InOrder(root->leftChild);
+    printf("%c ",root->data);
+    InOrder(root->rightChild);
+}
 
-int main(){
-	string str;
-	while(cin>>str){
-		int position = 0;
-		Node* root = BuildTree(position,str);
-		InOrder(root);
-		cout<<endl;
-	}
+int main() {
+    string str;
+    while(cin>>str) {
+        int position=0;
+        TreeNode *root=Build(position,str);
+        InOrder(root);
+        printf("\n");
+    }
 
-	return 0;
+    return 0;
 }
 
