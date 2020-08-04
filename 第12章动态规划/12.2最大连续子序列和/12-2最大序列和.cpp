ad2fe5cc2b3f443cf74916713c8af238a8a169f0
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-04 17:22:45 +0800

    Update 12-2最大序列和.cpp

--- 第12章动态规划/12.2最大连续子序列和/12-2最大序列和.cpp
+++ 第12章动态规划/12.2最大连续子序列和/12-2最大序列和.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+const int MAXN=1000000;
 
+int num[MAXN];
+
+int main() {
+    int n;
+    while(scanf("%d",&n)!=EOF) {
+        long long sum=-1,temp=0;
+        for(int i=0; i<n; i++) {
+            scanf("%d",&num[i]);
+            if(temp>0) {
+                temp+=num[i];
+            } else {
+                temp=num[i];
+            }
+            if(temp>sum) {
+                sum=temp;
+            }
+        }
+        printf("%lld\n",sum);
+    }
+    return 0;
+}
