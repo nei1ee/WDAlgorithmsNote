63452dc2a2e1d8c5bc70f54e9f5f713e61d53ef9
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-15 20:30:24 +0800

    Update 2-1abc.cpp

--- 第2章暴力破解/2.1枚举/2-1abc.cpp
+++ 第2章暴力破解/2.1枚举/2-1abc.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
-#include<cstdio>
 
 using namespace std;
 
-int main(){
-    for(int a=1;a<10;a++){
-        for(int b=0;b<10;b++){
-            for(int c=0;c<10;c++){
-                if(a*100+b*10+c+b*100+11*c==532){
+int main() {
+    for(int a=1; a<=9; a++) {
+        for(int b=0; b<=9; b++) {
+            for(int c=0; c<=9; c++) {
+                if(a*100+b*110+c*12==532) {
                     printf("%d %d %d\n",a,b,c);
                 }
             }
@@ -15,5 +14,3 @@ int main(){
     }
     return 0;
 }
-
-
