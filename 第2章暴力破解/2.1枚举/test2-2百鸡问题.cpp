c8b11e6e3f98ded0ba0a1e4d539dafacd71b07da
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-15 20:30:33 +0800

    Update test2-2百鸡问题.cpp

--- 第2章暴力破解/2.1枚举/test2-2百鸡问题.cpp
+++ 第2章暴力破解/2.1枚举/test2-2百鸡问题.cpp
@@ -1,25 +1,17 @@
 #include<iostream>
-#include<cstdio>
-
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
-    cin>>n;
-    for(int i=0; i<=100; i++)
-    {
-        for(int j=0; j<=100-i; j++)
-        {
-            int k=100-i-j;
-            if(i*5*3+j*3*3+k<=n*3)
-            {
-                cout<<"x="<<i<<",";
-                cout<<"y="<<j<<",";
-                cout<<"z="<<k<<endl;
+    while(scanf("%d",&n)!=EOF) {
+        for(int x=0; x<=100; x++) {
+            for(int y=0; y<=100-x; y++) {
+                int res=100-x-y;
+                if(15*x+9*y+res<=3*n) {
+                    printf("x=%d,y=%d,z=%d\n",x,y,res);
+                }
             }
         }
     }
-
     return 0;
 }
