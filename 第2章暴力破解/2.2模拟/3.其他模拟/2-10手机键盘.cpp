806fc69298b440b960c0db7ef4b01fcbe544f284
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-17 15:57:00 +0800

    更新2-9、2-10

--- 第2章暴力破解/2.2模拟/3.其他模拟/2-10手机键盘.cpp
+++ 第2章暴力破解/2.2模拟/3.其他模拟/2-10手机键盘.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
-
+#include<string.h>
 using namespace std;
-int keytab[26]= {1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,4,1,2,3,1,2,3,4};
+int keytab[26]= {
+    1,2,3,1,2,3,
+    1,2,3,1,2,3,1,2,3,
+    1,2,3,4,1,2,3,1,2,3,4
+};
 
 int main() {
-    char words[100]= {'\0'};
-    while(scanf("%s",words)!=EOF) {
+    string str;
+    while(cin>>str) {
         int time=0;
-        int i=0;
-        char temp=words[i++];
-        while(temp!='\0') {
-            //是否在一个按键上
-            if((words[i]-temp)==(keytab[words[i]-'a']-keytab[temp-'a'])) {
+        char temp;
+        for(int i=0; i<str.size(); i++) {
+            time+=keytab[str[i]-'a'];
+            if(i>0&&(str[i]-temp)==(keytab[str[i]-'a']-keytab[temp-'a'])) {
                 time+=2;
             }
-            time+=keytab[temp-'a'];
-            temp=words[i++];
+            temp=str[i];
         }
         printf("%d\n",time);
     }
--- 第2章暴力破解/2.2模拟/3.其他模拟/2-9剩下的树.cpp
+++ 第2章暴力破解/2.2模拟/3.其他模拟/2-9剩下的树.cpp
@@ -1,23 +1,27 @@
 #include<iostream>
-
 using namespace std;
-
 const int MAXL=10001;
-//bool型数组模拟马路，有树false，没树true
+//bool型数组模拟马路
 bool road[MAXL];
 
 int main() {
     int l,m;
-    while(scanf("%d %d",&l,&m)!=EOF) {
-        int trees=l+1;
-        while(m--) {
-            int left,right;
-            scanf("%d %d",&left,&right);
-            for(int i=left; i<=right; ++i) {
-                if(!road[i]) {
-                    road[i]=true;
-                    trees--;
+    while(scanf("%d%d",&l,&m)!=EOF) {
+        int left,right,trees=0;
+        for(int i=0; i<=l; i++) {
+            road[i]=true;
+        }
+        for(int i=0; i<m; i++) {
+            scanf("%d%d",&left,&right);
+            for(int j=left; j<=right; j++) {
+                if(road[j]) {
+                    road[j]=false;
                 }
+            }
+        }
+        for(int i=0; i<=l; i++) {
+            if(road[i]) {
+                trees++;
             }
         }
         printf("%d\n",trees);
