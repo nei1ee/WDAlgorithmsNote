d0d05800bf5c74b3708bcb122f2632bd17202e8e
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-17 16:49:52 +0800

    Update test3-7找位置.cpp

--- 第3章排序与查找/3.2查找/test3-7找位置.cpp
+++ 第3章排序与查找/3.2查找/test3-7找位置.cpp
@@ -6,21 +6,20 @@ int main() {
     string str;
     while(cin>>str) {
         for(int i=0; i<str.size(); i++) {
-            bool first=false;//重复字符的第一次出现
+            bool isFirst=false;
+            //如果字母已遍历
             if(str[i]=='*')continue;
             for(int j=i+1; j<str.size(); j++) {
                 if(str[i]==str[j]) {
-                    if(!first) {
-                        printf("%c:%d",str[i],i);
-                        first=true;
+                    if(!isFirst) {
+                        printf("%c:%d",str[j],i);
+                        isFirst=true;
                     }
                     printf(",%c:%d",str[j],j);
-                    str[j]='*';
+                    str[j]='*';//已遍历
                 }
             }
-            if(first) {
-                printf("\n");
-            }
+            if(isFirst)printf("\n");
         }
     }
     return 0;
