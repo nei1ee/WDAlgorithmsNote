046b639b0dee1645046f85392b183cf518ba9b8a
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-19 16:07:47 +0800

    更新第5章

--- 第5章数据结构一/5.3栈/5-4Zero-complexity Transposition.cpp
+++ 第5章数据结构一/5.3栈/5-4Zero-complexity Transposition.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-int main(){
-	int n;
-	stack<long long> num;
-	long long temp;
-	while(scanf("%d",&n)!=EOF){
-		for(int i=0;i<n;i++){
-			cin>>temp;
-			num.push(temp);
-		}
-		for(int i=0;i<n;i++){
-			cout<<num.top()<<" ";
-			num.pop();
-		}
-	}
-	return 0;
+int main() {
+    int n;
+    stack<long long> num;
+    long long temp;
+    while(scanf("%d",&n)!=EOF) {
+        for(int i=0; i<n; i++) {
+            cin>>temp;
+            num.push(temp);
+        }
+        for(int i=0; i<n; i++) {
+            cout<<num.top()<<" ";
+            num.pop();
+        }
+    }
+    return 0;
 }
