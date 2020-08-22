973ba70ed560b8d6cf1a85925f9a3e9c8128e2df
lazylzg <lizhiguo@cumt.edu.cn>
2020-08-22 10:50:27 +0800

    更新第七章

--- 第7章贪心策略/7.1简单贪心/7-1鸡兔同笼.cpp
+++ 第7章贪心策略/7.1简单贪心/7-1鸡兔同笼.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
-
 using namespace std;
-
 int main() {
     int a;
     while(scanf("%d",&a)!=EOF) {
-        int minimun=0,maximun=0;
-        if(a%2==0) {
-            minimun=a/4+(a%4)/2;
-            maximun=a/2;
+        int maxinum=0,mininum=0;
+        if(a%2==0){
+            maxinum=a/2;
+            mininum=a/4+(a%4)/2;
         }
-        printf("%d %d\n",minimun,maximun);
+        printf("%d %d\n",mininum,maxinum);
     }
     return 0;
 }
